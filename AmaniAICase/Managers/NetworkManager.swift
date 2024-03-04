6c3460612a9dbcd5a40ff14fb371df7f648bc49c
Y. Yılmaz Erdoğmuş <yyerdogmus@gmail.com>
2024-03-04 14:59:14 +0300

    NetworkManager has created

---
+++ AmaniAICase/Managers/NetworkManager.swift
@@ -0,0 +1,44 @@
+//
+//  NetworkManager.swift
+//  AmaniAICase
+//
+//  Created by Y. Yılmaz Erdoğmuş on 4.03.2024.
+//
+
+import Foundation
+
+class NetworkManager {
+    
+    static let shared = NetworkManager()
+    
+    private init() {}
+    
+    func fetchData(completion: @escaping (Result<ImageData, Error>) -> Void) {
+        guard let url = URL(string: "https://gist.githubusercontent.com/zekierciyas/48e744272a0aad671173409211ed716c/raw/de31c073f35250fa41022dd0a8422cf60758c4e9/images") else {
+            completion(.failure(NetworkError.invalidURL))
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data, error == nil else {
+                completion(.failure(error ?? NetworkError.unknownError))
+                return
+            }
+            
+            do {
+                let imageData = try JSONDecoder().decode(ImageData.self, from: data)
+                completion(.success(imageData))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+        
+        task.resume()
+    }
+    
+}
+
+enum NetworkError: Error {
+    case invalidURL
+    case unknownError
+}
